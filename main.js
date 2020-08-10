ddee84373549e15dea72ae7214cf6c07b2931a56
José Vieira <josecarlos8161@gmail.com>
2020-08-10 10:59:25 -0300

    First Window

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // create a window
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // load the index file
+  win.loadFile('index.html')
+
+  //win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
